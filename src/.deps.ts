f5f50dd4517f333f6a5e4a157d69812bface0644
mcgear <michael.gearhardt@gmail.com>
2025-10-06 10:39:42 -0600

    ref updates

--- src/.deps.ts
+++ src/.deps.ts
@@ -5,29 +5,29 @@ export {
   type EaCSharedSimulatorDetails,
   type EaCSimulatorAsCode,
   isEaCAzureIoTHubDataConnectionDetails,
-} from 'jsr:@o-industrial/common@0.0.475-integration/eac';
+} from 'jsr:@o-industrial/common@0.0.477/eac';
 export {
   DataConnection,
   DataConnectionModuleBuilder,
-} from 'jsr:@o-industrial/common@0.0.475-integration/fluent/connections';
+} from 'jsr:@o-industrial/common@0.0.477/fluent/connections';
 export {
   Simulator,
   SimulatorModuleBuilder,
-} from 'jsr:@o-industrial/common@0.0.475-integration/fluent/simulators';
-export { Step, StepModuleBuilder } from 'jsr:@o-industrial/common@0.0.475-integration/fluent/steps';
+} from 'jsr:@o-industrial/common@0.0.477/fluent/simulators';
+export { Step, StepModuleBuilder } from 'jsr:@o-industrial/common@0.0.477/fluent/steps';
 export {
   WarmQuery,
   WarmQueryModuleBuilder,
-} from 'jsr:@o-industrial/common@0.0.475-integration/fluent/warm-queries';
+} from 'jsr:@o-industrial/common@0.0.477/fluent/warm-queries';
 
 export {
   type AzureDataExplorerOutput,
   AzureDataExplorerOutputSchema,
-} from 'jsr:@o-industrial/common@0.0.475-integration/types';
+} from 'jsr:@o-industrial/common@0.0.477/types';
 
-export { shaHash } from 'jsr:@o-industrial/common@0.0.475-integration/utils/client';
+export { shaHash } from 'jsr:@o-industrial/common@0.0.477/utils/client';
 
-export { DataConnectionStatsSchema } from 'jsr:@o-industrial/oi-core-pack@0.0.12-integration/connections';
+export { DataConnectionStatsSchema } from 'jsr:@o-industrial/oi-core-pack@0.0.15/connections';
 
 export type { Status } from 'jsr:@fathym/common@0.2.274';
 export { z } from 'jsr:@fathym/common@0.2.274/third-party/zod';
